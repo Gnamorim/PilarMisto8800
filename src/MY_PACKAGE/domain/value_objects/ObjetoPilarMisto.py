d2849f7681718d242ed97f00d173439a7cec5849
Gnamorim <gui.amorim10@gmail.com>
2026-04-04 14:55:46 -0300

    inclusão de docstrings no objetopilarmisto

--- src/MY_PACKAGE/domain/value_objects/ObjetoPilarMisto.py
+++ src/MY_PACKAGE/domain/value_objects/ObjetoPilarMisto.py
@@ -6,6 +6,13 @@ from MY_PACKAGE.domain.value_objects.ObjetoAco import AcoEstrutural, AcoArmadura
 
 class ObjetoPilarMisto(ABC):
 
+    """
+    Esta é uma classe abstrata base, que carrega todos os métodos compartilhados entre os pilares mistos
+    Também estabelece contratos entre os nomes de propriedades e metodos necessários a todas as implementações concretas
+
+    Usado para o Service de flexo-compressão.
+    """
+
     #  inicializaco das variaves existentes em todos os modelos
     def __init__(
             self,
@@ -108,7 +115,7 @@ class ObjetoPilarMisto(ABC):
                 raise ValueError("cobrimento deve ser > 0")
         
       
-        # Valores físicos - limites construtivos -REFINAR
+        # Valores físicos - limites construtivos - REFINAR
 
         if self.diametro_armadura_longitudinal > 50:
             raise ValueError("diametro_armadura_longitudinal deve <= 50 mm")
@@ -141,6 +148,9 @@ class ObjetoPilarMisto(ABC):
 
     @abstractmethod
     def _limite_escopo(self):
+        """
+        Avalia o pilar dentro do escopo do metodo simplificado da norma. Comum a todos os pilares
+        """
         
         # --- area perfil >1% da area total ---
         area_aco = self.area_aco()
@@ -189,6 +199,9 @@ class ObjetoPilarMisto(ABC):
         pass
 
     def area_total(self):
+        """
+        Calcula a area total da seção tranversal do pilar
+        """
         return self.area_concreto() + self.area_aco() + self.area_armadura() 
 
     # momento de inercia
@@ -293,18 +306,24 @@ class ObjetoPilarMisto(ABC):
 
     @property
     def rigidez_axial_aco(self):
+        """Calcula a rigidez axial do aço estrutural"""
         return (self.area_aco * self.material_aco_estrutural.modulo_elasticidade)
 
     @property
     def rigidez_axial_concreto(self):
+        """Calcula a rigidez axial do concreto"""
         return (self.area_concreto * self.material_concreto.modulo_elasticidade_secante)
     
     @property
     def rigidez_axial_armadura(self):
+        """ Calcula a rigidez axial da armadura"""
         return (self.area_armadura * self.material_armadura.modulo_elasticidade)
     
     @property
     def rigidez_axial_equivalente(self):
+        """
+        Calcula a rigidez axial equivalente do pilar (EA)e
+        """
         return (self.rigidez_axial_aco + self.rigidez_axial_armadura + self.rigidez_axial_concreto)
     
     # flexão
@@ -316,6 +335,9 @@ class ObjetoPilarMisto(ABC):
 
     @property
     def rigidez_flexao_equivalente_x(self):
+        """
+        Calcula a rigidez equivalente a flexão (EI)e em relação ao eixo X
+        """
         aco = (self.momento_inercia_aco_x * self.material_aco_estrutural.modulo_elasticidade)
         armadura = (self.momento_inercia_armadura_x * self.material_armadura.modulo_elasticidade)
         concreto = self.alpha_c * (self.momento_inercia_concreto_x * self.material_concreto.modulo_elasticidade_secante)
@@ -324,6 +346,9 @@ class ObjetoPilarMisto(ABC):
     
     @property
     def rigidez_flexao_equivalente_y(self):
+        """
+        Calcula a rigidez equivalente a flexão (EI)e em relação ao eixo Y
+        """
         aco = (self.momento_inercia_aco_y * self.material_aco_estrutural.modulo_elasticidade)
         armadura = (self.momento_inercia_armadura_y * self.material_armadura.modulo_elasticidade)
         concreto = self.alpha_c * (self.momento_inercia_concreto_y * self.material_concreto.modulo_elasticidade_secante)
@@ -350,25 +375,43 @@ class ObjetoPilarMisto(ABC):
 
     # Aco
     def capacidade_axial_plastico_aco(self):
+        """
+        Calcula a capacidade axial plastica nominal do aço na seção transversal
+        """
         return self.area_aco() * self.material_aco_estrutural.fy
     
     def capacidade_axial_plastico_aco_design(self):
+        """
+        Calcula a capacidade axial plastica de design do aço na seção transversal
+        """
         return self.area_aco() * self.material_aco_estrutural.resistencia_design
 
 
     # concreto
     def capacidade_axial_plastico_concreto(self):
+        """
+        Calcula a capacidade axial plastica nominal do concreto na seção transversal
+        """
         return self.area_concreto() * self.material_concreto.fck
     
     def capacidade_axial_plastico_concreto_design(self):
+        """
+        Calcula a capacidade axial plastica de design do concreto na seção transversal
+        """
         return self.area_concreto() * self.fcd1
     
 
     # seção transversal
     def capacidade_axial_plastico(self):
+        """
+        Calcula a capacidade axial plastica nominal da seção transversal
+        """
         return (self.capacidade_axial_plastico_aco() + self.capacidade_axial_plastico_armadura() + self.capacidade_axial_plastico_concreto())
     
     def capacidade_axial_plastico_design(self):
+        """
+        Calcula a capacidade axial plastica de design da seção transversal
+        """
         return (self.capacidade_axial_plastico_aco_design() + self.capacidade_axial_plastico_armadura_design() + self.capacidade_axial_plastico_concreto_design())
     
     @property
@@ -381,8 +424,14 @@ class ObjetoPilarMisto(ABC):
     def capacidade_axial_resistente_secao_design(self):
         pass
 
+    # --- Capacidade resistente do pilar ---
+
     @property
     def carga_flambagem_elastica(self):
+        """
+        Calcula a carga de flexão elastica para ambos os eixos, saindo com a menor
+        Ne = min(pi^2 * (EI)e,i / (L^2))
+        """
         ne_x = ((pi ** 2) * self.rigidez_flexao_equivalente_x / (self.comprimento_pilar_destravado ** 2))
         ne_y = ((pi ** 2) * self.rigidez_flexao_equivalente_y / (self.comprimento_pilar_destravado ** 2))
 
@@ -390,11 +439,18 @@ class ObjetoPilarMisto(ABC):
 
     @property
     def indice_esbeltez_reduzido(self):
-        
+        """
+        Calcula a esbeltez reduzida do pilar (M.3.2)
+        (Np,R / Ne )^0.5
+        """
         return ((self.capacidade_axial_resistente_secao_nominal / self.carga_flambagem_elastica) ** 0.5)
     
     @property
     def indice_esbeltez_reduzido_limite(self):
+        """
+        Limite para consideração dos efeitos de retração e fluencia do concreto (M.3.3)
+        """
+
         return (
             (90 / pi) * ((self.capacidade_axial_resistente_secao_nominal / self.rigidez_axial_equivalente) ** 0.5)
         )
@@ -412,6 +468,10 @@ class ObjetoPilarMisto(ABC):
         
     @property
     def fator_reducao(self):
+
+        """
+        Calcula o fator de redução, conforme 5.3.3 na NBR 8800:2024
+        """
         if self.indice_esbeltez_reduzido <= 1.5:
             result = 0.68 ** (self.indice_esbeltez_reduzido ** 2)
         else:
@@ -421,10 +481,18 @@ class ObjetoPilarMisto(ABC):
     
     @property
     def capacidade_axial_resistente_pilar_nominal(self):
+        """ 
+        Calcula a capacidade axial resistente do pilar considerando capacidade nominal da seção (M.3.1)
+        NR = X * Np,R
+        """
         return (self.fator_reducao * self.capacidade_axial_resistente_secao_nominal)
     
     @property
     def capacidade_axial_resistente_pilar_design(self):
+        """ 
+        Calcula a capacidade axial resistente do pilar considerando capacidade de design da seção (M.3.1)
+        NRd = X * Np,Rd
+        """
         return (self.fator_reducao * self.capacidade_axial_resistente_secao_design)
 
     # --- Capacidades de Flexão ---
@@ -432,22 +500,42 @@ class ObjetoPilarMisto(ABC):
 
     @property
     def momento_resistente_plastico_aco_xx(self):
+        """
+        Calcula a capacidade resistente a flexão nominal no eixo X (M.5.5) do perfil de aço
+        Mpl,a,R = fy* (Za - Zan)
+        """
         return self.material_aco_estrutural.fy * (self.modulo_resistente_plastico_aco_x - self.modulo_resistente_plastico_aco_x_lnp)
 
     @property
     def momento_resistente_plastico_aco_yy(self):
+        """
+        Calcula a capacidade resistente a flexão nominal no eixo Y (M.5.5) do perfil de aço
+        Mpl,a,R = fy* (Za - Zan)
+        """
         return self.material_aco_estrutural.fy * (self.modulo_resistente_plastico_aco_y - self.modulo_resistente_plastico_aco_y_lnp)
 
     @property
     def momento_resistente_plastico_concreto_xx(self):
+        """
+        Calcula a capacidade resistente a flexão nominal no eixo X (M.5.5) da seção de concreto
+        Mpl,c,R = 0.5 * fck* (Zc - Zcn)
+        """
         return 0.5 * self.material_concreto.fck * (self.modulo_resistente_plastico_concreto_x - self.modulo_resistente_plastico_concreto_x_lnp)
 
     @property
     def momento_resistente_plastico_concreto_yy(self):
+        """
+        Calcula a capacidade resistente a flexão nominal no eixo Y (M.5.5) da seção de concreto
+        Mpl,c,R = 0.5 * fck* (Zc - Zcn)
+        """
         return 0.5 * self.material_concreto.fck * (self.modulo_resistente_plastico_concreto_y - self.modulo_resistente_plastico_concreto_y_lnp)
 
     @property
     def momento_resistente_plastico_armadura_xx(self):
+        """
+        Calcula a capacidade resistente a flexão nominal no eixo X (M.5.5) das armaduras da seção
+        Mpl,s,R = fys* (Zs - Zsn)
+        """
         if not self.material_armadura:
             return 0
 
@@ -455,6 +543,10 @@ class ObjetoPilarMisto(ABC):
 
     @property
     def momento_resistente_plastico_armadura_yy(self):
+        """
+        Calcula a capacidade resistente a flexão nominal no eixo Y (M.5.5) das armaduras da seção
+        Mpl,s,R = fys* (Zs - Zsn)
+        """
         if not self.material_armadura:
             return 0
 
@@ -462,10 +554,18 @@ class ObjetoPilarMisto(ABC):
 
     @property
     def momento_resistente_plastico_total_xx(self):
+        """
+        Calcula a capacidade resistente a flexão nominal no eixo X (M.5.5)
+        Mpl,R = Mpl,a,R + Mpl,c,R + Mpl,s,R
+        """
         return (self.momento_resistente_plastico_aco_xx + self.momento_resistente_plastico_concreto_xx + self.momento_resistente_plastico_armadura_xx)
 
     @property
     def momento_resistente_plastico_total_yy(self):
+        """
+        Calcula a capacidade resistente a flexão nominal no eixo Y (M.5.5)
+        Mpl,R = Mpl,a,R + Mpl,c,R + Mpl,s,R
+        """
         return (self.momento_resistente_plastico_aco_yy + self.momento_resistente_plastico_concreto_yy + self.momento_resistente_plastico_armadura_yy)
     
 
@@ -473,22 +573,42 @@ class ObjetoPilarMisto(ABC):
 
     @property
     def momento_resistente_plastico_aco_design_xx(self):
+        """
+        Calcula a capacidade resistente a flexão de design no eixo X (M.5.5) do perfil de aço
+        Mpl,a,R = fyd* (Za - Zan)
+        """
         return self.material_aco_estrutural.resistencia_design * (self.modulo_resistente_plastico_aco_x - self.modulo_resistente_plastico_aco_x_lnp)
 
     @property
     def momento_resistente_plastico_aco_design_yy(self):
+        """
+        Calcula a capacidade resistente a flexão de design no eixo Y (M.5.5) do perfil de aço
+        Mpl,a,R = fyd* (Za - Zan)
+        """
         return self.material_aco_estrutural.resistencia_design * (self.modulo_resistente_plastico_aco_y - self.modulo_resistente_plastico_aco_y_lnp)
 
     @property
     def momento_resistente_plastico_concreto_design_xx(self):
+        """
+        Calcula a capacidade resistente a flexão de design no eixo X (M.5.5) da seção de concreto
+        Mpl,c,Rd = 0.5 * fcd1* (Zc - Zcn)
+        """ 
         return 0.5 * self.fcd1 * (self.modulo_resistente_plastico_concreto_x - self.modulo_resistente_plastico_concreto_x_lnp)
 
     @property
     def momento_resistente_plastico_concreto_design_yy(self):
+        """
+        Calcula a capacidade resistente a flexão de design no eixo Y (M.5.5) da seção de concreto
+        Mpl,c,Rd = 0.5 * fcd1* (Zc - Zcn)
+        """         
         return 0.5 * self.fcd1 * (self.modulo_resistente_plastico_concreto_y - self.modulo_resistente_plastico_concreto_y_lnp)
 
     @property
     def momento_resistente_plastico_armadura_design_xx(self):
+        """
+        Calcula a capacidade resistente a flexão de design no eixo X (M.5.5) das armaduras da seção
+        Mpl,s,Rd = fsd* (Zs - Zsn)
+        """
         if not self.material_armadura:
             return 0
 
@@ -496,6 +616,10 @@ class ObjetoPilarMisto(ABC):
 
     @property
     def momento_resistente_plastico_armadura_design_yy(self):
+        """
+        Calcula a capacidade resistente a flexão de design no eixo Y (M.5.5) das armaduras da seção
+        Mpl,s,Rd = fsd* (Zs - Zsn)
+        """
         if not self.material_armadura:
             return 0
 
@@ -503,10 +627,18 @@ class ObjetoPilarMisto(ABC):
 
     @property
     def momento_resistente_plastico_total_design_xx(self):
+        """
+        Calcula a capacidade resistente a flexão de design no eixo X (M.5.5)
+        Mpl,Rd = Mpl,a,Rd + Mpl,c,Rd + Mpl,s,Rd
+        """
         return (self.momento_resistente_plastico_aco_design_xx + self.momento_resistente_plastico_concreto_design_xx + self.momento_resistente_plastico_armadura_design_xx)
 
     @property
     def momento_resistente_plastico_total_design_yy(self):
+        """
+        Calcula a capacidade resistente a flexão de design no eixo Y (M.5.5)
+        Mpl,Rd = Mpl,a,Rd + Mpl,c,Rd + Mpl,s,Rd
+        """
         return (self.momento_resistente_plastico_aco_design_yy + self.momento_resistente_plastico_concreto_design_yy + self.momento_resistente_plastico_armadura_design_yy)
     
 
@@ -515,22 +647,42 @@ class ObjetoPilarMisto(ABC):
 
     @property
     def momento_resistente_maximo_plastico_aco_xx(self):
+        """
+        Calcula a capacidade resistente máxima a flexão nominal no eixo X (M.5.5) do perfil de aço
+        Mmax,a,R = fy* (Za)
+        """
         return self.material_aco_estrutural.fy * (self.modulo_resistente_plastico_aco_x )
 
     @property
     def momento_resistente_maximo_plastico_aco_yy(self):
+        """
+        Calcula a capacidade resistente máxima a flexão nominal no eixo Y (M.5.5) do perfil de aço
+        Mmax,a,R = fy* (Za)
+        """
         return self.material_aco_estrutural.fy * (self.modulo_resistente_plastico_aco_y )
 
     @property
     def momento_resistente_maximo_plastico_concreto_xx(self):
+        """
+        Calcula a capacidade resistente máxima a flexão nominal no eixo X (M.5.5) do concreto na seção
+        Mmax,c,R = 0.5 * fck * (Zc)
+        """
         return 0.5 * self.material_concreto.fck * (self.modulo_resistente_plastico_concreto_x)
 
     @property
     def momento_resistente_maximo_plastico_concreto_yy(self):
+        """
+        Calcula a capacidade resistente máxima a flexão nominal no eixo Y (M.5.5) do concreto na seção
+        Mmax,c,R = 0.5 * fck * (Zc)
+        """
         return 0.5 * self.material_concreto.fck * (self.modulo_resistente_plastico_concreto_y)
 
     @property
     def momento_resistente_maximo_plastico_armadura_xx(self):
+        """
+        Calcula a capacidade resistente máxima a flexão nominal no eixo X (M.5.5) da armadura na seção
+        Mmax,s,R = fys * (Zs)
+        """
         if not self.material_armadura:
             return 0
 
@@ -538,6 +690,10 @@ class ObjetoPilarMisto(ABC):
 
     @property
     def momento_resistente_maximo_plastico_armadura_yy(self):
+        """
+        Calcula a capacidade resistente máxima a flexão nominal no eixo Y (M.5.5) da armadura na seção
+        Mmax,s,R = fys * (Zs)
+        """
         if not self.material_armadura:
             return 0
 
@@ -545,10 +701,18 @@ class ObjetoPilarMisto(ABC):
 
     @property
     def momento_resistente_maximo_plastico_total_xx(self):
+        """
+        Calcula a capacidade resistente máxima a flexão nominal no eixo X (M.5.5)
+        Mmax,R = Mmax,a,R + Mmax,c,R + Mmax,s,R
+        """
         return (self.momento_resistente_maximo_plastico_aco_xx + self.momento_resistente_maximo_plastico_concreto_xx + self.momento_resistente_maximo_plastico_armadura_xx)
 
     @property
     def momento_resistente_maximo_plastico_total_yy(self):
+        """
+        Calcula a capacidade resistente máxima a flexão nominal no eixo Y (M.5.5)
+        Mmax,R = Mmax,a,R + Mmax,c,R + Mmax,s,R
+        """
         return (self.momento_resistente_maximo_plastico_aco_yy + self.momento_resistente_maximo_plastico_concreto_yy + self.momento_resistente_maximo_plastico_armadura_yy)
     
 
@@ -556,22 +720,42 @@ class ObjetoPilarMisto(ABC):
 
     @property
     def momento_resistente_maximo_plastico_aco_design_xx(self):
+        """
+        Calcula a capacidade resistente máxima a flexão de design no eixo X (M.5.5) do perfil de aço
+        Mmax,a,Rd = fyd * (Za)
+        """
         return self.material_aco_estrutural.resistencia_design * (self.modulo_resistente_plastico_aco_x)
 
     @property
     def momento_resistente_maximo_plastico_aco_design_yy(self):
+        """
+        Calcula a capacidade resistente máxima a flexão de design no eixo Y (M.5.5) do perfil de aço
+        Mmax,a,Rd = fyd * (Za)
+        """
         return self.material_aco_estrutural.resistencia_design * (self.modulo_resistente_plastico_aco_y)
 
     @property
-    def momento_resistente_maximo_plastico_concreto_design_xx(self):
+    def momento_resistente_maximo_plastico_concreto_design_xx(self): 
+        """
+        Calcula a capacidade resistente máxima a flexão de design no eixo X (M.5.5) do concreto na seção
+        Mmax,c,Rd = 0.5 * fcd1 * (Zc)
+        """
         return 0.5 * self.fcd1 * (self.modulo_resistente_plastico_concreto_x)
 
     @property
     def momento_resistente_maximo_plastico_concreto_design_yy(self):
+        """
+        Calcula a capacidade resistente máxima a flexão de design no eixo Y (M.5.5) do concreto na seção
+        Mmax,c,Rd = 0.5 * fcd1 * (Zc)
+        """  
         return 0.5 * self.fcd1 * (self.modulo_resistente_plastico_concreto_y)
 
     @property
     def momento_resistente_maximo_plastico_armadura_design_xx(self):
+        """
+        Calcula a capacidade resistente máxima a flexão de design no eixo X (M.5.5) da armadura na seção
+        Mmax,s,Rd = fsd * (Zs)
+        """
         if not self.material_armadura:
             return 0
 
@@ -579,6 +763,10 @@ class ObjetoPilarMisto(ABC):
 
     @property
     def momento_resistente_maximo_plastico_armadura_design_yy(self):
+        """
+        Calcula a capacidade resistente máxima a flexão de design no eixo Y (M.5.5) da armadura na seção
+        Mmax,s,Rd = fsd * (Zs)
+        """
         if not self.material_armadura:
             return 0
 
@@ -586,10 +774,18 @@ class ObjetoPilarMisto(ABC):
 
     @property
     def momento_resistente_maximo_plastico_total_design_xx(self):
+        """
+        Calcula a capacidade resistente máxima a flexão de design no eixo X (M.5.5)
+        Mmax,Rd = Mmax,a,Rd + Mmax,c,Rd + Mmax,s,Rd
+        """
         return (self.momento_resistente_maximo_plastico_aco_design_xx + self.momento_resistente_maximo_plastico_concreto_design_xx + self.momento_resistente_maximo_plastico_armadura_design_xx)
 
     @property
     def momento_resistente_maximo_plastico_total_design_yy(self):
+        """
+        Calcula a capacidade resistente máxima a flexão de design no eixo Y (M.5.5)
+        Mmax,Rd = Mmax,a,Rd + Mmax,c,Rd + Mmax,s,Rd
+        """
         return (self.momento_resistente_maximo_plastico_aco_design_yy + self.momento_resistente_maximo_plastico_concreto_design_yy + self.momento_resistente_maximo_plastico_armadura_design_yy)
 
 
